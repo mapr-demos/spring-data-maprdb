db088e7f574535f1d4b585f14dcf87adaff0aa2f
Vlad-Storona <vstorona@cybervisiontech.com>
2018-09-10 18:20:14 +0300

    added some fixes

--- src/main/java/com/mapr/springframework/data/maprdb/repository/query/ConditionBasedMapRQuery.java
+++ src/main/java/com/mapr/springframework/data/maprdb/repository/query/ConditionBasedMapRQuery.java
@@ -24,9 +24,9 @@ public class ConditionBasedMapRQuery extends AbstractMapRQuery {
         QueryCondition condition = MapRDB.newCondition();
 
         for(PartTree.OrPart orPart : tree)
-            condition.or().condition(convertOrPartToQueryCondition(orPart, parametersIterator)).close();
+            condition.or().condition(convertOrPartToQueryCondition(orPart, parametersIterator)).close().build();
 
-        return condition.build();
+        return condition;
     }
 
     @Override
@@ -49,24 +49,27 @@ public class ConditionBasedMapRQuery extends AbstractMapRQuery {
         String name = part.getProperty().getSegment();
         QueryCondition condition;
 
+        Object parameters;
         switch(part.getType()) {
             case SIMPLE_PROPERTY:
-                if(part.getNumberOfArguments() == 1)
-                    condition = MapRDB.newCondition().is(name, QueryCondition.Op.EQUAL, itr.next().toString()).build();
+                parameters = itr.next();
+                if(!(parameters instanceof Collection<?>))
+                    condition = MapRDB.newCondition().is(name, QueryCondition.Op.EQUAL, parameters.toString()).build();
                 else
-                    condition = MapRDB.newCondition().equals(name, new ArrayList<>((Collection<?>) itr.next()));
+                    throw new UnsupportedOperationException(part.getType().toString() + " method with Example is not supported yet");
                 break;
             case NEGATING_SIMPLE_PROPERTY:
-                if(part.getNumberOfArguments() == 1)
-                    condition = MapRDB.newCondition().is(name, QueryCondition.Op.NOT_EQUAL, itr.next().toString()).build();
+                parameters = itr.next();
+                if(!(parameters instanceof Collection<?>))
+                    condition = MapRDB.newCondition().is(name, QueryCondition.Op.NOT_EQUAL, parameters.toString()).build();
                 else
-                    condition = MapRDB.newCondition().notEquals(name, new ArrayList<>((Collection<?>) itr.next()));
+                    throw new UnsupportedOperationException(part.getType().toString() + " method with Example is not supported yet");
                 break;
             case LIKE:
-                condition = MapRDB.newCondition().like(part.getProperty().getSegment(), itr.next().toString()).build();
+                condition = MapRDB.newCondition().like(name, itr.next().toString()).build();
                 break;
             case NOT_LIKE:
-                condition = MapRDB.newCondition().notLike(part.getProperty().getSegment(), itr.next().toString()).build();
+                condition = MapRDB.newCondition().notLike(name, itr.next().toString()).build();
                 break;
             case IN:
                 condition =  MapRDB.newCondition().in(name, new ArrayList<>((Collection<?>) itr.next())).build();
@@ -74,23 +77,20 @@ public class ConditionBasedMapRQuery extends AbstractMapRQuery {
             case NOT_IN:
                 condition = MapRDB.newCondition().notIn(name, new ArrayList<>((Collection<?>) itr.next())).build();
                 break;
-            case IS_NULL:
+            case EXISTS:
                 condition = MapRDB.newCondition().exists(name).build();
                 break;
-            case IS_NOT_NULL:
-                condition = MapRDB.newCondition().notExists(name).build();
-                break;
             case LESS_THAN:
-                condition = MapRDB.newCondition().is(name, QueryCondition.Op.LESS, itr.next().toString()).build();
+                condition = MapRDB.newCondition().is(name, QueryCondition.Op.LESS, Double.parseDouble(itr.next().toString())).build();
                 break;
             case LESS_THAN_EQUAL:
-                condition = MapRDB.newCondition().is(name, QueryCondition.Op.LESS_OR_EQUAL, itr.next().toString()).build();
+                condition = MapRDB.newCondition().is(name, QueryCondition.Op.LESS_OR_EQUAL, Double.parseDouble(itr.next().toString())).build();
                 break;
             case GREATER_THAN:
-                condition = MapRDB.newCondition().is(name, QueryCondition.Op.GREATER, itr.next().toString()).build();
+                condition = MapRDB.newCondition().is(name, QueryCondition.Op.GREATER, Double.parseDouble(itr.next().toString())).build();
                 break;
             case GREATER_THAN_EQUAL:
-                condition = MapRDB.newCondition().is(name, QueryCondition.Op.GREATER_OR_EQUAL, itr.next().toString()).build();
+                condition = MapRDB.newCondition().is(name, QueryCondition.Op.GREATER_OR_EQUAL, Double.parseDouble(itr.next().toString())).build();
                 break;
             case TRUE:
                 condition = MapRDB.newCondition().is(name, QueryCondition.Op.EQUAL, true).build();
