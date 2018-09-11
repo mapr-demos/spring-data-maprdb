5c20227222002b687144c6d8130b26ee84fa51ad
Vlad-Storona <vstorona@cybervisiontech.com>
2018-09-11 10:57:42 +0300

    removed classes

--- src/main/java/com/mapr/springframework/data/maprdb/core/query/MapRParameterAcessor.java
+++
@@ -1,6 +0,0 @@
-package com.mapr.springframework.data.maprdb.core.query;
-
-import org.springframework.data.repository.query.ParameterAccessor;
-
-public interface MapRParameterAcessor extends ParameterAccessor {
-}
