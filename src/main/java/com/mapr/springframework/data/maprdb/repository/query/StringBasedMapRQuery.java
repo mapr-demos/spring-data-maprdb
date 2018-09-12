0ee5a6c04100f753b0fe5f90c44acbf5842d6f74
Vlad-Storona <vstorona@cybervisiontech.com>
2018-09-12 18:04:27 +0300

    added string class for string based queries

---
+++ src/main/java/com/mapr/springframework/data/maprdb/repository/query/StringBasedMapRQuery.java
@@ -0,0 +1,29 @@
+package com.mapr.springframework.data.maprdb.repository.query;
+
+import com.mapr.springframework.data.maprdb.core.MapROperations;
+import org.ojai.store.Query;
+import org.springframework.data.repository.query.QueryMethod;
+
+public class StringBasedMapRQuery extends AbstractMapRQuery{
+
+    protected String query;
+
+    public StringBasedMapRQuery(MapRQueryMethod method, MapROperations operations) {
+        this(method.getAnnotatedQuery(), method, operations);
+    }
+
+    public StringBasedMapRQuery(String query, MapRQueryMethod method, MapROperations operations) {
+        super(method, operations);
+        this.query = query;
+    }
+
+    @Override
+    protected Query convertToQuery(Object[] parameters) {
+        return operations.getConnection().newQuery().where(query).build();
+    }
+
+    @Override
+    public QueryMethod getQueryMethod() {
+        return null;
+    }
+}
