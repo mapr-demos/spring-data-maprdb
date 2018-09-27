8f812470863fb640bc3822207f62d14960542461
dmitriyHavrilovich <dhavrilovich@cybervisiontech.com>
2018-09-27 11:08:56 +0300

    added TODO and warnings

--- src/main/java/com/mapr/springframework/data/maprdb/repository/query/AbstractMapRQuery.java
+++ src/main/java/com/mapr/springframework/data/maprdb/repository/query/AbstractMapRQuery.java
@@ -2,11 +2,14 @@ package com.mapr.springframework.data.maprdb.repository.query;
 
 import com.mapr.springframework.data.maprdb.core.MapROperations;
 import org.ojai.store.Query;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.data.repository.query.QueryMethod;
 import org.springframework.data.repository.query.RepositoryQuery;
 import java.util.List;
 
 public abstract class AbstractMapRQuery implements RepositoryQuery {
+    private final static Logger LOGGER = LoggerFactory.getLogger(AbstractMapRQuery.class);
 
     protected final MapROperations operations;
     protected final MapRQueryMethod method;
@@ -54,11 +57,17 @@ public abstract class AbstractMapRQuery implements RepositoryQuery {
         if(method.isStreamQuery())
             return records.stream();
 
-        if(isCountQuery())
+        // TODO Make normal count queries
+        if(isCountQuery()) {
+            LOGGER.warn("Count queries are working not in optimal way, please don't use them without necessity");
             return records.size();
+        }
 
-        if(isExistsQuery())
+        // TODO Make normal exists queries
+        if(isExistsQuery()) {
+            LOGGER.warn("Exists queries are working not in optimal way, please don't use them without necessity");
             return records.size() > 0;
+        }
 
         return records.size() > 0 ? records.get(0) : null;
     }
